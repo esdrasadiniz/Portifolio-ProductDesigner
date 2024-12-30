e32e2875e83d9ffeb36a51e4667b8d80244c96e0
esdrasadiniz <esdras.pert@gmail.com>
2024-12-30 18:45:30 -0300

    correções botao

--- JS/banner-nav.js
+++ JS/banner-nav.js
@@ -1,30 +1,31 @@
 const portifolioLabel = document.querySelector('label[for="option1"]');
 const portifolioLabelAnchor = document.querySelector('.option.-option1 a');
 const hiremeLabel = document.querySelector('label[for="option2"]');
-const hiremeLabelAchor = document.querySelector('.option.-option2 a');
+const hiremeLabelAnchor = document.querySelector('.option.-option2 a');
 const portifolioLabelArrow = document.querySelector('.option.-option1 a img');
 const hiremeLabelArrow = document.querySelector('.option.-option2 a img');
+
+portifolioLabelArrow.style.display = 'block';
+portifolioLabelAnchor.classList.add('active');
 
 
 hiremeLabel.addEventListener(
     'mouseover', () => {
-        portifolioLabelAnchor.classList.add('hireme-label');
         hiremeLabelArrow.style.display = 'block';
-        portifolioLabelArrow.style.display = 'none';
-        hiremeLabelAchor.classList.add('active');
+        hiremeLabelAnchor.classList.add('active');
         hiremeLabel.classList.add('active');
+        portifolioLabelArrow.style.display = 'none';
         portifolioLabelAnchor.classList.remove('active');
-        hiremeLabelAchor.style.justifyContent = 'center';
     }
 );
 
 hiremeLabel.addEventListener(
     'mouseout', () => {
-        portifolioLabelAnchor.classList.remove('hireme-label');
-        portifolioLabelArrow.style.display = 'block';
         hiremeLabelArrow.style.display = 'none';
+        hiremeLabelAnchor.classList.remove('active');
+        hiremeLabel.classList.remove('active');
+        portifolioLabelArrow.style.display = 'block';
         portifolioLabelAnchor.classList.add('active');
-        hiremeLabelAchor.classList.remove('active');
     }
 );
 
